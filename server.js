79e1ba8228aceee06980b463364c9af95343fe5c
jmo <40837639+jmo927@users.noreply.github.com>
2019-01-25 21:30:28 -0600

    fixed PORT

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 var exphbs = require("express-handlebars");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Initialize Express
 const app = express();
